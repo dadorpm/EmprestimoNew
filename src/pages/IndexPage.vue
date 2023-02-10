9ce74853d6c6d929ec9b492f910be67b135601e0
Adauto Meireles Dibay <dadorpm@hotmail.com>
2023-02-10 10:57:38 -0300

    adicionando animação transiction

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,18 +1,24 @@
 <template>
-  <q-page class="flex column flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/faced.png"
-      style="width: 300px; height: 160px"
-    >
-  <h4 class="">Sistema de Geração de Termo de Empréstimo</h4>
-  </q-page>
+  <transition
+    appear
+    enter-active-class="animated zoomIn"
+    leave-active-class="animated zoomOut"
+  >
+    <q-page class="flex column flex-center">
+      <img
+        alt="Quasar logo"
+        src="~assets/faced.png"
+        style="width: 300px; height: 160px"
+      />
+      <h4 class="">Sistema de Geração de Termo de Empréstimo</h4>
+    </q-page>
+  </transition>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
